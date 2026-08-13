0b409ebb81e3098ee86e9dfe23f66264d9632231
Grigoriy Re:Re:Re <rgrigoriy2412@gmail.com>
2026-08-13 19:01:23 +0300

    fix[domain]: extend exceptions + add repo docs

--- src/hobby_tracker/domain/activity/repository.py
+++ src/hobby_tracker/domain/activity/repository.py
@@ -7,8 +7,27 @@ from .activity import Activity
 class ActivityRepository(Protocol):
     def add(self, hobby: Activity) -> None: ...
 
+    """
+    Raises:
+        ActivityAttributeDuplicate if UUID already exists
+    """
+
     def get_by_id(self, id: UUID) -> Activity: ...
+
+    """
+    Repository must track itself all the changes with loaded entities like Python list
+
+    Raises:
+        ActivityNotFound(id) if activity not found
+    """
 
     def exists(self, id: UUID) -> bool: ...
 
     def delete(self, hobby: Activity) -> None: ...
+
+    """
+    Only tracking activity is allowed to delete
+    
+    Raises:
+        ActivityDeleteError if try to delete activity not from Repository
+    """
--- src/hobby_tracker/domain/exceptions.py
+++ src/hobby_tracker/domain/exceptions.py
@@ -4,3 +4,23 @@ class HobbyTrackerException(Exception):
 
 class HobbyNotFound(HobbyTrackerException):
     pass
+
+
+class HobbyAttributeDuplicate(HobbyTrackerException):
+    pass
+
+
+class HobbyDeleteError(HobbyTrackerException):
+    pass
+
+
+class ActivityNotFound(HobbyTrackerException):
+    pass
+
+
+class ActivityAttributeDuplicate(HobbyTrackerException):
+    pass
+
+
+class ActivityDeleteError(HobbyTrackerException):
+    pass
